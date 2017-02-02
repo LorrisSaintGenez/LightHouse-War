5a7b079732bf80efd74580f0f2d49495e6773ca5
Hadrien lamotte <hadrien.lamotte@gmail.com>
2017-02-02 22:11:18 +0100

    first commit hadrien

---
+++ Assets/Scripts/cameraFollowUser.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollowUser : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 v3T = transform.position;
+		v3T.z = player.transform.position.z + offset.z;
+		v3T.x = player.transform.position.x + offset.x;
+		transform.position = v3T;
+	}
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate  () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (-moveVertical, 0.0f, moveHorizontal);
+
+		rb.AddForce (movement * speed);
+	}
+}
