c13fb97f89ea929985f428f7cc7b7b8445b20328
Lorris <lorris.saint-genez@epita.fr>
2017-02-03 16:05:38 +0100

    Push force tamere

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -17,25 +17,34 @@ public class player : MonoBehaviour {
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 	}
-	
-    void Update () {
+
+    void Update() {
         Fire();
     }
 
-	// Update is called once per frame
-	void FixedUpdate () {
+    // Update is called once per frame
+    void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		Vector3 movement = new Vector3 (moveVertical, 0.0f, -moveHorizontal);
 
-		rb.AddForce (movement * speed);
-	}
+        Vector3 accel = movement * speed;
+
+		rb.AddForce (accel);
+    }
 
     void Fire () {
-        if (Input.GetButton("Fire1") && Time.time > nextFire) {
+        if (Input.GetKey(KeyCode.Mouse0) && Time.time > nextFire) {
             nextFire = Time.time + fireRate;
-            Rigidbody newBarrel = Instantiate(barrel, barrelSpawn.position, barrelSpawn.rotation) as Rigidbody;
+            Rigidbody newBarrel = Instantiate(barrel, new Vector3(barrelSpawn.position.x, barrelSpawn.position.y + 5, barrelSpawn.position.z), new Quaternion(barrelSpawn.rotation.x, barrelSpawn.rotation.y, barrelSpawn.rotation.z, barrelSpawn.rotation.w)) as Rigidbody;
+            newBarrel.velocity = -15f * speed * barrelSpawn.right;
+        }
+
+        if (Input.GetKey(KeyCode.Mouse1) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Rigidbody newBarrel = Instantiate(barrel, new Vector3(barrelSpawn.position.x, barrelSpawn.position.y + 5, barrelSpawn.position.z), new Quaternion(barrelSpawn.rotation.x, barrelSpawn.rotation.y, barrelSpawn.rotation.z + 30, barrelSpawn.rotation.w)) as Rigidbody;
             newBarrel.velocity = -50f * barrelSpawn.right;
         }
     }
