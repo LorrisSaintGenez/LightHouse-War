327f15ca15b78ba851f06eefe570dd0e2d8b207d
Hadrien lamotte <hadrien.lamotte@gmail.com>
2017-02-04 16:32:16 +0100

    add tower rotation particules and fires

---
+++ Assets/Scripts/Alexandrie.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alexandrie : MonoBehaviour {
+
+	public Light spotlightOne;
+	public Light spotlightTwo;
+	public Light spotlightThree;
+	public Light spotlightFour;
+	public Light point;
+
+	public Light BaseOne;
+	public Light BaseTwo;
+	public Light BaseThree;
+	public Light BaseFour;
+
+	public Rigidbody Barrel;
+	public GameObject explosion;
+	public Transform SpawnTowerOne;
+
+	public Transform SpawnTowerTwo;
+
+	public Transform SpawnTowerThree;
+
+	public Transform SpawnTowerFour;
+
+	public float SpawnTime;
+
+	// 0 no One, 1 player One, 2 player Two
+	private int winner;
+
+	// Use this for initialization
+	void Start () {
+		winner = 0;
+		changeUser (winner);
+		InvokeRepeating("TowerFire", 5, 5);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey(KeyCode.A))
+		{
+			changeUser (0);
+		}
+		if (Input.GetKey(KeyCode.B))
+		{
+			changeUser (1);
+		}
+		if (Input.GetKey(KeyCode.C))
+		{
+			changeUser (2);
+		}
+
+	}
+
+	void TowerFire(){
+		float force = Random.Range (5f, 20f);
+		Vector3 up = new Vector3 (0, Random.Range(5f, 15f), 0);
+		Vector3 vect = new Vector3 (0, 1, -1);
+		Rigidbody newBarrel = Instantiate(Barrel, new Vector3(SpawnTowerOne.position.x, SpawnTowerOne.position.y, SpawnTowerOne.position.z), SpawnTowerOne.rotation) as Rigidbody;
+		Instantiate(explosion, new Vector3(SpawnTowerOne.position.x, SpawnTowerOne.position.y, SpawnTowerOne.position.z), SpawnTowerOne.rotation);
+		newBarrel.velocity = force * -SpawnTowerOne.forward + up;
+
+		Vector3 vect1 = new Vector3 (1, 1, 0);
+		Rigidbody newBarrel1 = Instantiate(Barrel, new Vector3(SpawnTowerTwo.position.x, SpawnTowerTwo.position.y, SpawnTowerTwo.position.z), SpawnTowerTwo.rotation) as Rigidbody;
+		Instantiate (explosion, new Vector3 (SpawnTowerTwo.position.x, SpawnTowerTwo.position.y, SpawnTowerTwo.position.z), SpawnTowerTwo.rotation);
+		newBarrel1.velocity = force * SpawnTowerTwo.right + up;
+
+		Vector3 vect2 = new Vector3 (-1, 1, 0);
+		Rigidbody newBarrel2 = Instantiate(Barrel, new Vector3(SpawnTowerThree.position.x, SpawnTowerThree.position.y, SpawnTowerThree.position.z), SpawnTowerThree.rotation) as Rigidbody;
+		Instantiate(explosion, new Vector3(SpawnTowerThree.position.x, SpawnTowerThree.position.y, SpawnTowerThree.position.z), SpawnTowerThree.rotation);
+		newBarrel2.velocity = force * -SpawnTowerThree.right + up;
+
+		Vector3 vect3 = new Vector3 (0 ,1, 1);
+		Rigidbody newBarrel3 = Instantiate(Barrel, new Vector3(SpawnTowerFour.position.x, SpawnTowerFour.position.y, SpawnTowerFour.position.z), SpawnTowerFour.rotation) as Rigidbody;
+		Instantiate(explosion, new Vector3(SpawnTowerFour.position.x, SpawnTowerFour.position.y, SpawnTowerFour.position.z), SpawnTowerFour.rotation);
+		print (SpawnTowerFour.forward);
+		newBarrel3.velocity = force * SpawnTowerFour.forward + up;
+	}
+
+
+	void changeUser(int val){
+		winner = val;
+		Color color;
+		if (val == 0) {
+			color = Color.white;
+		} else if (val == 1) {
+			color = Color.red;
+		} else {
+			color = Color.green;
+		}
+		ApplyColor (color);
+	}
+
+	void ApplyColor(Color color)
+	{
+		spotlightOne.color = color;
+		spotlightTwo.color = color;
+		spotlightThree.color = color;
+		spotlightFour.color = color;
+		point.color = color;
+		BaseOne.color = color;
+		BaseTwo.color = color;
+		BaseThree.color = color;
+		BaseFour.color = color;
+	}
+}
---
+++ Assets/Scripts/RotateTower.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateTower : MonoBehaviour {
+
+	bool rotatingRight;
+	float speed;
+	int count;
+	bool plain;
+
+	// Use this for initialization
+	void Start () {
+		if (Random.Range (0, 100) > 50) {
+			rotatingRight = true;
+		} else {
+			rotatingRight = false;
+		}
+		plain = false;
+		speed = Random.Range (0.0f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (transform.rotation);
+		if (rotatingRight) {
+			transform.Rotate (0, speed, 0, Space.World);
+			count++;
+		} else {
+			transform.Rotate (0, -speed, 0, Space.World);
+			count++;
+		}
+		if (count > 500 && !plain || count > 1000 && plain) {
+			plain = true;
+			count = 0;
+			print ("Change");
+			rotatingRight = !rotatingRight;
+		}
+
+	}
+}
