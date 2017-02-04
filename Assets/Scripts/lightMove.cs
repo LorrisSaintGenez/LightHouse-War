884442fe39b9cbf257a8b5fda221172dbfe56a31
Hadrien lamotte <hadrien.lamotte@gmail.com>
2017-02-04 02:08:53 +0100

    awesome features

---
+++ Assets/Scripts/lightMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightMove : MonoBehaviour {
+
+	public float angle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate(0, angle * Time.deltaTime, 0, Space.World);
+	}
+}
